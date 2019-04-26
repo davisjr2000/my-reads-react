1d3650c0257dee92c72d30675f13bc5160b3b53f
Davis JR <davisjr2000@hotmail.com>
2019-04-26 11:26:27 -0300

    Display books using search component

--- src/Search.js
+++ src/Search.js
@@ -1,8 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import * as BooksAPI from './BooksAPI'
 
 class Search extends React.Component {
+  state = {
+    books: []
+  }
+
+  handleChange = (e) => {
+    console.log(1)
+    let query = e.target.value
+    if (query !== ""){
+      BooksAPI.search(query).then(result => this.setState({
+        books: result
+      }))
+    }
+  }
    render(){
        return (
         <div className="app">
@@ -10,20 +23,32 @@ class Search extends React.Component {
             <div className="search-books-bar">
               <Link className="close-search" to="/">Close</Link>
               <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
+                <input type="text" placeholder="Search by title or author" onChange={this.handleChange}/>
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+                {this.state.books.map((book) =>
+                    <li>
+                    <div className="book">
+                    <div className="book-top">
+                      <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                        <div className="book-shelf-changer">
+                        <select onChange={(e) => {this.updateBook(e, book.id)}} defaultValue={book.shelf}> 
+                            <option value="move" disabled>Move to...</option>
+                            <option value="currentlyReading">Currently Reading</option>
+                            <option value="wantToRead">Want to Read</option>
+                            <option value="read">Read</option>
+                            <option value="none">None</option>
+                        </select>
+                        </div>
+                      </div>
+                      <div className="book-title">{book.title}</div>
+                      <div className="book-authors">{book.author}</div>
+                      </div>
+                      </li>
+                    )}
+                  </ol>
             </div>
           </div>
         </div>
